3c6dbfd5d99546e7e181f19bff2a7f75b1367bc2
Maria-Vaht <96318594+Maria-Vaht@users.noreply.github.com>
2022-10-30 16:37:08 +0300

    Content translated into English; submit handling added

--- js/animations.js
+++ js/animations.js
@@ -2,14 +2,14 @@ const INCREASE_NUMBER_ANIMATION_SPEED = 40;
 
 
 function increaseNumberAnimationStep(i, element, endNumber) {
-    if (i <= endNumber) {
-      if (i === endNumber)
-        element.innerText = i + "+";
-      else
-        element.innerText = i;
-    }
-    i += 100;
-    setTimeout(increaseNumberAnimationStep, INCREASE_NUMBER_ANIMATION_SPEED, i, element, endNumber);
+  if (i <= endNumber) {
+    if (i === endNumber)
+      element.innerText = i + "+";
+    else
+      element.innerText = i;
+  }
+  i += 100;
+  setTimeout(increaseNumberAnimationStep, INCREASE_NUMBER_ANIMATION_SPEED, i, element, endNumber);
 }
 
 function initIncreaseNumberAnimation() {
@@ -32,7 +32,7 @@ function updateScroll() {
 
   let windowBottomPosition = window.scrollY + window.innerHeight;
   let countElementPosition = document.querySelector(".features__clients-count").offsetTop;
-  if (windowBottomPosition >= countElementPosition  && !animationInited) {
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
     animationInited = true;
     initIncreaseNumberAnimation();
   }
@@ -46,13 +46,13 @@ document.querySelector("#budget").addEventListener("change", function handleSele
     const formContainer = document.createElement("div");
     formContainer.classList.add("form__group");
     formContainer.classList.add("form__other-input");
- 
+
     const input = document.createElement("input");
-    input.placeholder = "Введите ваш вариант";
+    input.placeholder = "Enter your option";
     input.type = "text";
- 
+
     formContainer.appendChild(input);
-    document.querySelector(".form form").insertBefore(formContainer, document.querySelector(".form__submit")); 
+    document.querySelector(".form form").insertBefore(formContainer, document.querySelector(".form__submit"));
   }
   else {
     const otherInput = document.querySelector(".form__other-input");
@@ -67,7 +67,7 @@ function addSmoothScroll(anchor) {
   anchor.addEventListener("click", event => {
     event.preventDefault();
     document.querySelector(event.target.getAttribute("href")).scrollIntoView({
-    behavior: "smooth"
+      behavior: "smooth"
     });
   })
 }
@@ -77,3 +77,12 @@ document.querySelectorAll("a[href^='#']").forEach(anchor => {
 })
 
 addSmoothScroll(document.querySelector(".more-button"));
+
+
+function handleSubmit(e) {
+  e.preventDefault()
+}
+
+document.querySelector(".form__block").addEventListener("submit", function handleSubmit(event) {
+  event.preventDefault()
+})
